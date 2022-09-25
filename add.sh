7d9f43c7c1b664bd43a208b1ad2c9a552e850310
elangovan0 <elangovan.k@hotmail.com>
2022-09-25 14:29:56 +0000

    commit-This add program which ihave cloned from arun's ITC repo

--- add.sh
+++ add.sh
@@ -1,5 +1,6 @@
 echo ""
 echo "shell program to add 2 values"
+echo "This add program which ihave cloned from arun's ITC repo"
 echo ""
 read -p "enter the value of a is: " a
 read -p "enter the value of b is: " b
